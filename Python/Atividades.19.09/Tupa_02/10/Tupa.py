809fecd26a06ed45b41bafb445aa15c06654d701
VulgoTG <gselvacaquin@gmail.com>
2025-10-02 22:02:18 -0300

    .

---
+++ Python/Atividades.19.09/Tupa_02/10/Tupa.py
@@ -0,0 +1,19 @@
+#Tupas
+compras = ("arroz", "feijão", "alho", "leite", "k9", "porra")
+#print(compras[1])
+#print(compras[0:2])
+#print(len(compras))
+#for c in compras:
+    #print(c)
+#for c in range(0,len(compras)):
+    #print(c)
+#for c in range(0,len(compras)):
+    #print(compras,c)
+#for c in range(len(compras)):
+    #print("esse é os seus produtos em ordem: ", c+1, compras[c])
+#for item in compras:
+    #print("Eu vou comprar", item)
+#for c in range(0, len(compras)):
+    #print("eu vou comprar",compras[c],"na posição",c)
+#for pos, item in enumerate(compras):
+    #print("eu vou comprar",item,"na posição",pos)
---
+++ Python/Atividades.19.09/Tupa_02/10/Tupa2.py
@@ -0,0 +1,6 @@
+a = (2,5,4)
+b = (5,8,1,2)
+c = a+b
+print(c)
+#print(c.count(4)) #mostra quantos tem
+#print(c.index(4)) #mostra em qual posição
