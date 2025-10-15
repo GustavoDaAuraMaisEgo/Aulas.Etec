593ba71fc555a70679b254319665543c794dece9
VulgoTG <gselvacaquin@gmail.com>
2025-10-15 19:25:58 -0300

    .

---
+++ Python/Tupa_02.10/exemplos.py
@@ -0,0 +1,26 @@
+#Tupas
+#compras = ("arroz", "feijão", "alho", "leite")
+#print(compras[1])
+#print(compras[0:2])
+#print(len(compras))
+#for c in compras:
+    #print(c)
+#for c in range(0,len(compras)):
+    #print(c)
+#for c in range(0,len(compras)):
+    #print(compras,c)
+#for c in range(len(compras)):
+    #print("esse é os seus produtos em ordem: ", c+1, compras[c])
+#for item in compras:
+    #print("Eu vou comprar", item)
+#for c in range(0, len(compras)):
+    #print("eu vou comprar",compras[c],"na posição",c)
+#for pos, item in enumerate(compras):
+    #print("eu vou comprar",item,"na posição",pos)
+
+a = (2,5,4)
+b = (5,8,1,2)
+c = a+b
+print(c)
+#print(c.count(4)) #mostra quantos tem
+#print(c.index(4)) #mostra em qual posição
