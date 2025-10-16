3acbc9660e32d9818aea1fad1f8c31ed6c2ff57c
Gustavo Valim <gselvacaquin@gmail.com>
2025-10-16 20:12:34 -0300

    .

--- Python/Aula_ex2_16.10/Exercicio2.py
+++ Python/Aula_ex2_16.10/Exercicio2.py
@@ -1,14 +1,17 @@
 user = "Professor"
 senha = "1234"
-contador = ""
+contador = "s"  
+
+print("Boas-vindas à tela de login!")
+
 while contador == "s":
-    print("Boas-vindas à tela de login!")
+    user_novo = input("Qual seu usuário?: ")
+    senha_novo = input("Qual a sua senha?: ")
 
-    user_novo = str(input("Qual seu usuário?: "))
-    senha_novo = str(input("Qual a sua senha?: "))
-
-    if user_novo == user and senha == senha_novo:
-        print("deu certo")
+    if user_novo == user and senha_novo == senha:
+        
+        break  
     else:
-        contador = str(input("Tentar novamente? s/n"))
-        print("caralho")
+        print("Usuário ou senha incorretos.")
+        contador = input("\nTentar novamente? (s/n): ").lower()
+        print("Obrigado por usar nosso programa.")
